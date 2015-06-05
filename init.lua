f1f3c15b64313332b8eb2d0479c9bcaa6662832b
bobombolo <danjor@halfstrong.com>
2015-06-05 01:19:27 -0600

    beginning of rewrite, not working

--- init.lua
+++ init.lua
@@ -20,6 +20,11 @@ automata.check_list = {} -- indexed by pos_to_string(pos) and value = true (@tod
 --each round of checking needs an id
 automata.check_count = 0
 
+--moving away from ABMs we need our own node list
+automata.inactive_cell_registry = {} -- indexed by pos_string
+automata.active_cell_registry={} -- indexed by fingerprint, value is {ttl, rules_hash, cell_list} , cell_list is indexed by pos_string
+automata.rulesets={} -- indexed by rules_hash which is just serialized rules table
+
 -- function to add nodes to the iteration queue
 local function enqueue(pos, data)
 	--pos is passed as a table but we need it as a string for indexing
@@ -70,7 +75,7 @@ local timer = 0
 minetest.register_globalstep(function(dtime)
 	timer = timer + dtime;
 	if timer >= 5 then
-		-- process the queue
+		-- 1. process the queue
 		automata:process_queue()
 		timer = 0
 		--minetest.log("action", "block_queue: "..dump(automata.block_queue))
@@ -81,6 +86,15 @@ minetest.register_globalstep(function(dtime)
 			if v == automata.check_count then automata.check_list[k]= nil end -- wipe all entries from last check round
 		end
 		automata.check_count = automata.check_count + 1 
+		-- 2. grow all active cells
+		--loop through each pattern in the active cell registry and do grow() on each node
+		for fingerprint,values in pairs(automata.active_cell_registry) do
+			--one pattern at a time
+
+			for pos_string,_ in pairs(values.cell_list) do --cell_list is indexed by pos_string
+				grow(pos_string, fingerprint, values.ttl, minetest.unserialize(values.rules_hash)) --these needed for enqueue
+			end
+		end
 	end
 end)
 
@@ -91,7 +105,7 @@ end)
 local function list_neighbors(pos, neighbors, plane, fingerprint, ttl)
 	--minetest.log("action", "neighbors: "..neighbors..", plane: "..plane..", print: "..fingerprint..", ttl: "..ttl)
 	local list = {}
-	if neighbors == 5 or neighbors == 9 then
+	if neighbors == 4 or neighbors == 9 then -- von Neumann neighborhood
 		if plane == "x" then --actually the plane yz
 			list.n  = {x=pos.x,  y=pos.y+1,  z=pos.z}
 			list.e  = {x=pos.x,  y=pos.y,    z=pos.z+1}
@@ -111,7 +125,7 @@ local function list_neighbors(pos, neighbors, plane, fingerprint, ttl)
 			--something went wrong
 		end
 	end
-	if neighbors == 9 then
+	if neighbors == 8 then -- Moore neighborhood
 		if plane == "x" then
 			list.ne = {x=pos.x,y=pos.y+1,z=pos.z+1}
 			list.se = {x=pos.x,y=pos.y-1,z=pos.z+1}
@@ -146,50 +160,40 @@ local function list_neighbors(pos, neighbors, plane, fingerprint, ttl)
 		end
 	end
 	-- mark the node as checked
-	automata.check_list[minetest.pos_to_string(pos)] = automata.check_count --causing problems
+	automata.check_list[minetest.pos_to_string(pos)] = automata.check_count --causing problems?
 	--minetest.log("action", "count active: "..same_count..", count inactive: "..#inactive_neighbors)
 	return same_count, inactive_neighbors
 end
 
 --new metadata based grow function
-local function grow(pos)
+local function grow(pos_string, fingerprint, rules)
+	
 	--first we see if this node has never been checked
-	if automata.check_list[minetest.pos_to_string(pos)] ~= automata.check_count then 
+	if automata.check_list[pos_string] ~= automata.check_count then 
 		--minetest.log("action", "not already checked")
-
-		local meta			= minetest.get_meta(pos)
-		--minetest.log("action", dump(meta:to_table()))
-		
-		local ttl			= meta:get_int("ttl")
-		local fingerprint 	= meta:get_int("fingerprint")
-		local binrules 		= meta:get_string("binrules") --trailing zeroes lost if int
-		local neighbors 	= meta:get_int("neighbors")
-		local growth 		= meta:get_int("growth")
-		local plane 		= meta:get_string("plane")
-		local trail			= meta:get_string("trail")
-		
+				
 		--now we must count the neighbors, identify how many are the same and which ones are not
-		local same_count, inactive_neighbors = list_neighbors(pos, neighbors, plane, fingerprint, ttl) --marks this node as checked
+		local same_count, inactive_neighbors = list_neighbors(minetest.string_to_pos(pos_string), fingerprint) --marks this node as checked
 		
 		--survival rules for this node applied
 		--minetest.log("action", "before survival rules: "..binrules)
 		--minetest.log("action", "newpos before survival rules: "..minetest.pos_to_string(pos))
-		if string.sub(binrules, same_count*2+2, same_count*2+2) == "1" then
+		if string.find(rules.survive, same_count) then
 			
 			local newpos = {}
-			if     plane == "x" then newpos = {x=pos.x+growth, y=pos.y, z=pos.z}
-			elseif plane == "y" then newpos = {x=pos.x, y=pos.y+growth, z=pos.z}
-			elseif plane == "z" then newpos = {x=pos.x, y=pos.y, z=pos.z+growth}
+			if     rules.plane == "x" then newpos = {x=pos.x+rules.growth, y=pos.y, z=pos.z}
+			elseif rules.plane == "y" then newpos = {x=pos.x, y=pos.y+rules.growth, z=pos.z}
+			elseif rules.plane == "z" then newpos = {x=pos.x, y=pos.y, z=pos.z+rules.growth}
 			end
 			--minetest.log("action", "newpos after survival rules: "..minetest.pos_to_string(newpos))
 			
 			--if growth is set for this node, then we not only enqueue the next gen, but we set the old block to die
-			if growth ~= 0 then enqueue(pos, trail) end --passing a string implies death
+			if growth ~= 0 then enqueue_death(pos, fingerprint) end --passing a string implies death
 			--regardless of growth setting we enqueue the next generation for life
-			enqueue(newpos, meta:to_table()) --passing a table implies life
+			enqueue_life(newpos, fingerprint) --passing a table implies life
 		else
 			--if survival fails we enqueue current cell for death
-			enqueue(pos, trail) --passing a string implies death
+			enqueue_death(pos, fingerprint) --passing a string implies death
 		end
 		
 		--birth rules for all inactive neighbors checked
@@ -197,21 +201,31 @@ local function grow(pos)
 			for _,v in pairs(inactive_neighbors) do
 				if automata.check_list[minetest.pos_to_string(v)] ~= automata.check_count --already checked
 				and automata.block_queue[minetest.pos_to_string(v)] == nil then --already has a destiny
-					local sc, _ = list_neighbors(v, neighbors, plane, fingerprint, ttl) --will mark the node as checked
+					local sc, _ = list_neighbors(v, fingerprint) --will mark the node as checked
 					--based on the birth rules turn this new cell on
-					if string.sub(binrules, sc*2+1, sc*2+1) == "1" then
+					if string.find(rules.birth, sc) then
 						local newpos = {}
-						if     plane == "x" then newpos = {x=v.x+growth, y=v.y, z=v.z}
-						elseif plane == "y" then newpos = {x=v.x, y=v.y+growth, z=v.z}
-						elseif plane == "z" then newpos = {x=v.x, y=v.y, z=v.z+growth}
+						if     rules.plane == "x" then newpos = {x=v.x+rules.growth, y=v.y, z=v.z}
+						elseif rules.plane == "y" then newpos = {x=v.x, y=v.y+rules.growth, z=v.z}
+						elseif rules.plane == "z" then newpos = {x=v.x, y=v.y, z=v.z+rules.growth}
 						end
-						enqueue(newpos, meta:to_table()) --passing a table implies life
+						enqueue_life(newpos, fingerprint) --passing a table implies life
 					end
 				end
 			end
 		end
 	end
 	return true --end of the grow function
+end
+
+-- force load a block (taken from technic)
+-- if the node is loaded, returns it. If it isn't loaded, load it and return nil.
+function get_or_load_node(pos)
+	local node_or_nil = minetest.get_node_or_nil(pos)
+	if node_or_nil then return node_or_nil end
+	local vm = VoxelManip()
+	local MinEdge, MaxEdge = vm:read_from_map(pos, pos)
+	return nil
 end
 
 -- a generic node type for the new metadata-based growth function
@@ -225,109 +239,12 @@ minetest.register_node("automata:active", {
 	},
 })
 
--- automata rule Conway node -- registering this explicitly so that 
-minetest.register_node("automata:conway", {
-	description = "Conway's Game of Life",
-	tiles = {"conway.png"},
-	light_source = 5,
-	groups = {	live_automata = 1, --abm applied to this group only
-				oddly_breakable_by_hand=1,
-				--not_in_creative_inventory = 1 --only programmable nodes appear in the inventory
-	},
-	on_construct = function(pos)
-		--local n = minetest.get_node(pos)
-		local meta = minetest.get_meta(pos)
-		meta:set_string("nodename", "automata:conway")
-		meta:set_string("infotext", "\"Conway's Game of Life\"")
-		meta:set_string("binrules", "000001110000000000")
-		meta:set_int("neighbors", 9)
-		meta:set_int("growth", 1) -- @todo will add this to the formspec
-		meta:set_string("plane", "y") -- @todo will add this to the formspec
-		meta:set_int("ttl", 50)
-		meta:set_string("trail", "air")
-		meta:set_string("final", "air") --might not use anymore
-		meta:set_int("fingerprint", 1111111) --conway blocks are no use unless they are all the same
-	end,
-})
-
--- automata generic growth action for any live automata
-minetest.register_abm({
-	nodenames = {"group:live_automata"},
-	neighbors = {"air"}, --won't grow underground or underwater . . .
-	interval = 4,
-	chance = 1,
-	action = function(pos)
-		--minetest.log("action", "grow triggered")
-		return grow(pos)
-	end,
-})
 
 --[[  FOR THE CREATION OF A PROGRAMMABLE BLOCK,
       AND IT'S ACTIVATION AS A LIVE AUTOMATA BLOCK --]]
 
--- function to convert integer to binary string
-local function toBits(num, bits)
-    -- returns a table of bits, most significant first.
-    bits = bits or select(2,math.frexp(num))
-    local t={} -- will contain the bits        
-    for b=1,bits,1 do --left to right binary table
-        t[b]=math.fmod(num,2)
-        num=(num-t[b])/2
-    end
-    return t
-end
-
-local function nks_rule_convert(nkscode)
-
-	local bits = 0
-	local neighbors = string.sub(nkscode, 1, 1) --very important that the nodename starts with "automata:9n" or "automata:5n"
-	-- the 5 or 9 neighbor type
-	if neighbors ~= "5" and neighbors ~= "9" then
-		minetest.log("error", "node name not in correct format for nks_rule_convert()")
-		return false
-	else
-		neighbors = tonumber(neighbors)
-	end
-	
-	-- get the integer code from the nodename
-	local code = string.sub(nkscode, 3) --very important that the nodename continues with a code only, no trailing chars
-	local intcode = 0
-	
-	-- also we know that a 5n rule can be no larger than 1023, though a 9n rule can be less than 1024
-	if code then intcode = tonumber(code) else return false end
-	--minetest.debug("action", "nodename= "..name)
-	if not intcode or (neighbors == 5 and intcode > 1023) or (neighbors == 9 and intcode > 262143) then 
-		minetest.log("error", "improperly formatted code -- must be in the format 5n2 to 5n1022 or 9n1024 to 9n262142 even only")
-		return false
-	end
-	
-	-- convert the integer code to a bigendian binary table
-	local bintable = toBits(intcode, neighbors*2)
-	--minetest.log("action", table.concat(bintable))
-	
-	-- test for survival rule 0, which cannot be implemented in this mod
-	if bintable[1] == 1 then
-		minetest.log("error", "odd-numbered codes are not supported")
-		return false
-	end
-	-- test for single-node growth
-	--[[if bintable[2] == 0 then
-		minetest.chat_send_all("please note, this code will only die alone")
-	end--]]
-	local str = ""
-	--pass the binary rules back to the node definition
-	for i=1,1,1 do
-		str = str..bintable[i]
-	end
-	--minetest.log("action", "binstring: "..str)
-	local binstring = tostring(table.concat(bintable))
-	--minetest.log("action", "concat: "..binstring)
-	return neighbors, binstring
-	
-end
-
--- new block that requires starts inactive and takes input
-minetest.register_node("automata:programmable", {
+-- new block that requires activation
+minetest.register_node("automata:inactive", {
 	description = "Programmable Automata",
 	tiles = {"dead.png"},
 	light_source = 5,
@@ -336,51 +253,105 @@ minetest.register_node("automata:programmable", {
 	on_construct = function(pos)
 		--local n = minetest.get_node(pos)
 		local meta = minetest.get_meta(pos)
-		meta:set_string("formspec", "size[8,8]" ..
-				"field[1,1;4,1;text;NKS Code (eg: 5n942);]" ..
-				"field[1,2;4,1;plane;Plane (x, y, or z);]" ..
-				"field[1,3;4,1;growth;Growth (-1, 0, 1, 2 ...);]" ..
-				"field[1,4;4,1;trail;Trail Block (eg: default:dirt);]" ..
-				"field[1,5;4,1;final;Final Block (eg: default:mese);]" ..
-				"field[1,6;4,1;ttl;Generations (eg: 30);]" ..
-				"button_exit[1,7;2,1;exit;Activate]"
-		) -- this alone makes the block right-clickable?
 		meta:set_string("infotext", "\"Inactive Automata\"")
+		--register the cell in the cell registry
+		automata.inactive_cell_registry[minetest.pos_to_string(pos)] = true -- the cell_registry will change this to the rule hash when active
 	end,
-	on_receive_fields = function(pos, formname, fields, sender) --or is it this that makes it clickable?
-		--print("Sign at "..minetest.pos_to_string(pos).." got "..dump(fields))
-		if minetest.is_protected(pos, sender:get_player_name()) then
-			minetest.record_protection_violation(pos, sender:get_player_name())
-			return
+	
+})
+
+-- the controller for activating cells
+minetest.register_tool("automata:remote" , {
+	description = "Automata RC",
+	inventory_image = "nks942.png",
+	on_use = function (itemstack, user, pointed_thing)
+		local pname = user:get_player_name()
+		
+		--make sure the inactive cell registry is not empty
+		if next(automata.inactive_cell_registry) then
+		minetest.show_formspec(pname, "automata:rc_form",
+			"size[8,9]" ..
+			"field[1,1;4,1;neighbors;Neighbors (4 or 8);]" ..
+			"field[1,5;4,1;code;Rules (eg: 2/23);]" ..
+			
+			"field[1,2;4,1;plane;Plane (x, y, or z);]" ..
+			"field[1,3;4,1;growth;Growth (-1, 0, 1, 2 ...);]" ..
+			"field[1,4;4,1;trail;Trail Block (eg: default:dirt);]" ..
+			"field[1,5;4,1;final;Final Block (eg: default:mese);]" ..
+			"field[1,6;4,1;ttl;Generations (eg: 30);]" ..
+			"button_exit[1,7;2,1;exit;Activate]")
+		else
+			minetest.chat_send_player(pname, "There are no inactive cells placed to activate!")
 		end
-		local meta = minetest.get_meta(pos)
-		if not fields.text then return end
-		
-		minetest.log("action", (sender:get_player_name() or "").." wrote \""..fields.text..
-				"\" to programmable automata at "..minetest.pos_to_string(pos))
-		meta:set_string("text", fields.text)
-		meta:set_string("infotext", '"'..fields.text..'"')
-		
-		--see if the entered data is a valid NKS rule
-		local neighbors, binrules = nks_rule_convert(fields.text)
-		if not neighbors or not binrules then 
-			minetest.log("error", "neighbors or binrules not returned")
-			return false
-		end
-		
-		--convert this block to an Active Automata and set meta
-		minetest.set_node(pos, {name="automata:active"})
-		--meta = minetest.get_meta(pos) --reload the meta now that we set a new node (not necessary)
-		meta:set_string("nodename", "automata:active") --this meta needed so that node never needs to be passed
-		meta:set_string("infotext", '"'..fields.text..'"')
-		meta:set_string("binrules", binrules)
-		meta:set_int("neighbors", neighbors)
-		
-		meta:set_int("growth", fields.growth and 1 or fields.growth)
-		meta:set_string("plane", fields.plane and "y" or fields.plane)
-		meta:set_int("ttl", fields.ttl and 22 or fields.ttl) -- this sets the limit, counts down each generation / iteration
-		meta:set_string("trail", fields.trail and "default:dirt" or fields.trail)
-		meta:set_string("final", fields.final and "default:dirt" or fields.final)
-		meta:set_int("fingerprint", math.random(1,100000)) --to test which neighbors count as neighbors
 	end,
 })
+
+-- Processing the form from the RC
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+	if formname == "automata:rc_form" then
+		-- form validation
+		local rules_hash = rules_validate(fields) --will be false if rules don't validate
+		if rules_hash then
+			local cell_list = {}
+			--activate all inactive nodes @todo handle this with voxelmanip
+			for pos_string,_ in pairs(automata.inactive_cell_registry) do --@todo check ownership of node? lock registry?
+				minetest.set_node(minetest.string_to_pos(pos_string), {name="automata:active"})
+				table.insert(cell_list, pos_string)
+				--wipe the inactive cell registry
+				automata.inactive_cell_registry[pos_string] = nil
+			end
+			--create a unique fingerprint for this activated set
+			local fingerprint = math.random(1,100000)
+			--in case we won the lottery and this fingerprint is already in use...
+			while automata.active_cell_registry[fingerprint] do
+				local fingerprint = math.random(1,100000)
+			end
+			--add the cell list to the active cell registry with the ttl, rules hash, and cell list
+			automata.active_cell_registry[fingerprint] = {ttl=fields.ttl, rules_hash=rules_hash, cell_list=cell_list}
+			
+			minetest.chat_send_player(player:get_player_name(), "You activated all inactive cells!")
+			return true
+		else
+			minetest.chat_send_player(player:get_player_name(), "Something was wrong with your inputs!")
+		end
+	end
+	
+end)
+
+function rules_validate(fields) --
+	local rules = {}
+	--local pname = user:get_player_name()
+	--minetest.chat_send_all("here :"..dump(fields))
+	
+	fields.code = fields.code and "2/23" or fields.code
+	local split = string.find(fields.code, "/")
+	if split then
+		-- take the values to the left and the values to the right @todo validation will be made moot by a stricter form
+		rules["birthrules"] = string.sub(fields.code, 1, split-1)
+		rules["suriviverules"] = string.sub(fields.code, split+1)
+		
+	else
+		--minetest.chat_send_player(pname, "the rule code should be in the format \"2/23\"")
+		return false
+	end
+	
+	rules["neighbors"] = fields.neighbors and 8 or fields.neighbors
+	rules["ttl"] = fields.ttl and 30 or fields.ttl
+	rules["growth"] = fields.growth and 0 or fields.growth
+	rules["plane"] = fields.plane and "y" or fields.growth
+	rules["trail"] = fields.trail and "air" or fields.trail
+	rules["final"] = fields.final and "stone" or fields.final
+	
+	return rules
+end
+
+
+
+-- an activated automata block -- further handling of this node done by globalstep
+minetest.register_node("automata:active", {
+	description = "Active Automata",
+	tiles = {"conway.png"},
+	light_source = 5,
+	groups = {live_automata = 1, oddly_breakable_by_hand=1},
+})
+
