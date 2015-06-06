587def911fb1a8f3b4a7c9bf953f0f0b658c33c3
bobombolo <danjor@halfstrong.com>
2015-06-06 02:42:52 -0600

    some testing done

--- init.lua
+++ init.lua
@@ -78,56 +78,58 @@ function automata.grow(pattern_id)
 	local new_cell_list = {} --the final cell list to transfer back to automata.patterns[pattern_id]
 							 -- some of ^ will be cells that survived in a growth=0 ruleset
 							 -- ^ this is to save the time of setting nodes and meta for survivals
-	local new_pmin, new_pmax = 0
+	local new_pmin, new_pmax = {x=0,y=0,z=0}
 	--load the rules
 	local rules = automata.rule_registry[automata.patterns[pattern_id].rule_id]
+	minetest.log("error", "line 84 = rules: "..dump(rules))
 	if automata.patterns[pattern_id].iteration == rules.ttl then
 		local is_final = true
 	else
 		local is_final = false
 	end
+	minetest.log("error", "iteration: "..automata.patterns[pattern_id].iteration)
 	local neighborhood= {}
 	local growth_offset = {}
 	-- determine neighborhood and growth offsets
 	if rules.neighbors == 4 or rules.neighbors == 8 then -- von Neumann neighborhood
 		if rules.plane == "x" then --actually the plane yz
-			growth_offset = {x = rules.growth}
-			neighborhood.n  = {y=  1}
-			neighborhood.e  = {z=  1}
-			neighborhood.s  = {y= -1}
-			neighborhood.w  = {z= -1}
+			growth_offset = {x = rules.growth, y=0, z=0}
+			neighborhood.n  = {x=  0,y=  1,z=  0}
+			neighborhood.e  = {x=  0,y=  0,z=  1}
+			neighborhood.s  = {x=  0,y= -1,z=  0}
+			neighborhood.w  = {x=  0,y=  0,z= -1}
 		elseif rules.plane == "y" then --actually the plane xz
-			growth_offset = {y = rules.growth}
-			neighborhood.n  = {z=  1}
-			neighborhood.e  = {x=  1}
-			neighborhood.s  = {z= -1}
-			neighborhood.w  = {x= -1}
+			growth_offset = {x=0, y = rules.growth, z=0}
+			neighborhood.n  = {x=  0,y=  0,z=  1}
+			neighborhood.e  = {x=  1,y=  0,z=  0}
+			neighborhood.s  = {x=  0,y=  0,z= -1}
+			neighborhood.w  = {x= -1,y=  0,z=  0}
 		elseif rules.plane == "z" then --actually the plane xy
-			growth_offset = {z = rules.growth}
-			neighborhood.n  = {y=  1}
-			neighborhood.e  = {x= -1}
-			neighborhood.s  = {y= -1}
-			neighborhood.w  = {x=  1}
+			growth_offset = {x=0, y=0, z = rules.growth}
+			neighborhood.n  = {x=  0,y=  1,z=  0}
+			neighborhood.e  = {x= -1,y=  0,z=  0}
+			neighborhood.s  = {x=  0,y= -1,z=  0}
+			neighborhood.w  = {x=  1,y=  0,z=  0}
 		else
 			--something went wrong
 		end
 	end
 	if rules.neighbors == 8 then -- add missing Moore neighborhood corners
 		if rules.plane == "x" then
-			neighborhood.ne = {y=  1,z=  1}
-			neighborhood.se = {y= -1,z=  1}
-			neighborhood.sw = {y= -1,z= -1}
-			neighborhood.nw = {y=  1,z= -1}
+			neighborhood.ne = {x=  0,y=  1,z=  1}
+			neighborhood.se = {x=  0,y= -1,z=  1}
+			neighborhood.sw = {x=  0,y= -1,z= -1}
+			neighborhood.nw = {x=  0,y=  1,z= -1}
 		elseif rules.plane == "y" then
-			neighborhood.ne = {x=  1,z=  1}
-			neighborhood.se = {x=  1,z= -1}
-			neighborhood.sw = {x= -1,z= -1}
-			neighborhood.nw = {x= -1,z=  1}
+			neighborhood.ne = {x=  1,y=  0,z=  1}
+			neighborhood.se = {x=  1,y=  0,z= -1}
+			neighborhood.sw = {x= -1,y=  0,z= -1}
+			neighborhood.nw = {x= -1,y=  0,z=  1}
 		elseif rules.plane == "z" then
-			neighborhood.ne = {x= -1,y=  1}
-			neighborhood.se = {x= -1,y= -1}
-			neighborhood.sw = {x=  1,y= -1}
-			neighborhood.nw = {x=  1,y=  1}
+			neighborhood.ne = {x= -1,y=  1,z=  0}
+			neighborhood.se = {x= -1,y= -1,z=  0}
+			neighborhood.sw = {x=  1,y= -1,z=  0}
+			neighborhood.nw = {x=  1,y=  1,z=  0}
 		else
 			--minetest.log("error", "neighbors: "..neighbors.." is invalid")
 		end
@@ -150,11 +152,11 @@ function automata.grow(pattern_id)
 		end
 		--now we have a same neighbor count, apply life and death rules
 		local gpos = {}
+		--minetest.log("error", "line 154 = rules.survive: ".. rules.survive..", same_count: "..same_count)
 		if string.find(rules.survive, same_count) then
 			--add to life list
-			for k,v in next, growth_offset do --only really need to do this once
-				gpos = {x=pos.x+offset.x, y=pos.y+offset.y, z=pos.z+offset.z}
-			end
+			gpos = {x=pos.x+growth_offset.x, y=pos.y+growth_offset.y, z=pos.z+growth_offset.z}
+
 			if rules.growth ~= 0 then
 				table.insert(life_list, gpos) --when node is actually set we will add to new_cell_list
 				table.insert(death_list, pos) --with growth, the old pos dies leaving rules.trail
@@ -165,7 +167,7 @@ function automata.grow(pattern_id)
 				else
 					new_cell_list[pos_hash] = true --if growth=0 we just let it be but add to new_cell_list
 					--oh, we also have to test it against the pmin and pmax
-					do k,v in next, pos do
+					for k,v in next, pos do
 						if pos.k < new_pmin.k then new_pmin.k = pos.k end
 						if pos.k > new_pmax.k then new_pmax.k = pos.k end
 					end
@@ -179,6 +181,7 @@ function automata.grow(pattern_id)
 	end
 	--loop through the new total neighbors list
 	for i,epos in next, empty_neighbors do
+		local same_count = 0
 		for k, offset in next, neighborhood do
 			--add the offsets to the position @todo although this isn't bad
 			local npos = {x=epos.x+offset.x, y=epos.y+offset.y, z=epos.z+offset.z}
@@ -191,11 +194,8 @@ function automata.grow(pattern_id)
 		local bpos = {}
 		if string.find(rules.birth, same_count) then
 			--add to life list
-			for k,v in next, growth_offset do --only really need to do this once
-				bpos = {x=epos.x+offset.x, y=epos.y+offset.y, z=epos.z+offset.z}
-			end
-				table.insert(life_list, bpos) --when node is actually set we will add to new_cell_list
-			end
+			bpos = {x=epos.x+growth_offset.x, y=epos.y+growth_offset.y, z=epos.z+growth_offset.z}
+			table.insert(life_list, bpos) --when node is actually set we will add to new_cell_list
 		end
 	end
 	
@@ -204,7 +204,8 @@ function automata.grow(pattern_id)
 		minetest.set_node(dpos, {name=rules.trail})
 	end
 	--set the nodes for births
-	for k,bpos in next, life_list do
+	minetest.log("action", "life_list: "..dump(life_list))
+	for k,bpos in next, life_list do --@todo why is this processing an empty table life_list!?
 		--test for final iteration
 		if is_final then
 			minetest.set_node(bpos, {name=rules.final})
@@ -214,7 +215,7 @@ function automata.grow(pattern_id)
 			meta:set_int("pattern_id", pattern_id)
 			--add to cell_list
 			table.insert(new_cell_list, minetest.node_position_to_hash(bpos))
-			do k,v in next, bpos do
+			for k,v in next, bpos do
 				if bpos.k < new_pmin.k then new_pmin.k = bpos.k end
 				if bpos.k > new_pmax.k then new_pmax.k = bpos.k end
 			end
@@ -252,7 +253,7 @@ function automata.rules_validate(fields) --
 	if split then
 		-- take the values to the left and the values to the right @todo validation will be made moot by a stricter form
 		rules["birth"] = string.sub(fields.code, 1, split-1)
-		rules["surivive"] = string.sub(fields.code, split+1)
+		rules["survive"] = string.sub(fields.code, split+1)
 		
 	else
 		--minetest.chat_send_player(pname, "the rule code should be in the format \"2/23\"")
@@ -308,7 +309,7 @@ minetest.register_node("automata:active", {
 -- new block that requires activation
 minetest.register_node("automata:inactive", {
 	description = "Programmable Automata",
-	tiles = {"dead.png"},
+	tiles = {"inactive.png"},
 	light_source = 5,
 	groups = {oddly_breakable_by_hand=1},
 	
@@ -317,7 +318,7 @@ minetest.register_node("automata:inactive", {
 		local meta = minetest.get_meta(pos)
 		meta:set_string("infotext", "\"Inactive Automata\"")
 		--register the cell in the cell registry
-		table.insert(automata.inactive_cells[minetest.hash_node_position(pos)] = true
+		automata.inactive_cells[minetest.hash_node_position(pos)] = true
 	end,
 	on_dig = function(pos)
 		--remove from the inactive cell registry (should be called by set_node)
@@ -328,24 +329,24 @@ minetest.register_node("automata:inactive", {
 -- an activated automata block -- further handling of this node done by globalstep
 minetest.register_node("automata:active", {
 	description = "Active Automata",
-	tiles = {"conway.png"},
-	drop = { max_items = 1, items = { "automata.inactive" } } -- change back to inactive when dug 
+	tiles = {"active.png"},
+	drop = { max_items = 1, items = { "automata.inactive" } }, -- change back to inactive when dug 
 	light_source = 5,
 	groups = {live_automata = 1, oddly_breakable_by_hand=1},
-	
 	on_dig = function(pos)
 		--get the pattern ID from the meta and remove the cell from the pattern table
 		local meta=minetest.get_meta(pos)
 		local pattern_id = meta:get_int("pattern_id")
 		automata.patterns[pattern_id].cell_list[minetest.hash_node_position(pos)] = nil
+		--@todo also check for pmin/pmax change
 		automata.patterns[pattern_id].cell_count = automata.patterns[pattern_id].cell_count -1
-	end
+	end,
 })
 
 -- the controller for activating cells
 minetest.register_tool("automata:remote" , {
-	description = "Automata RC",
-	inventory_image = "nks942.png",
+	description = "Automata Trigger",
+	inventory_image = "remote.png",
 	on_use = function (itemstack, user, pointed_thing)
 		local pname = user:get_player_name()
 		
@@ -353,8 +354,8 @@ minetest.register_tool("automata:remote" , {
 		if next(automata.inactive_cells) then
 		minetest.show_formspec(pname, "automata:rc_form",
 			"size[8,9]" ..
-			"field[1,1;4,1;neighbors;Neighbors (4 or 8);]" ..
-			"field[1,5;4,1;code;Rules (eg: 2/23);]" ..
+			"field[1,1;2,1;neighbors;Neighbors (4 or 8);]" ..
+			"field[3,1;4,1;code;Rules (eg: 2/23);]" ..
 			
 			"field[1,2;4,1;plane;Plane (x, y, or z);]" ..
 			"field[1,3;4,1;growth;Growth (-1, 0, 1, 2 ...);]" ..
@@ -377,21 +378,22 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 			--create the new pattern id empty
 			table.insert(automata.patterns, true) --placeholder to get id
 			local pattern_id = #automata.patterns
-			
+			local pos = {}
 			local cell_list = {}
 			--activate all inactive nodes @todo handle this with voxelmanip
 			for pos_hash,_ in pairs(automata.inactive_cells) do --@todo check ownership of node? lock registry?
-				minetest.set_node(minetest.get_position_from_hash(pos_hash), {name="automata:active"})
+				pos = minetest.get_position_from_hash(pos_hash)
+				minetest.set_node(pos, {name="automata:active"})
 				cell_list[pos_hash] = true
 				--wipe the inactive cell registry
 				automata.inactive_cells[pos_hash] = nil -- might not need this with after_destruct()
 				--add the pattern id to the metadata of each node we just converted
-				local meta = minetest.get_meta()
-				meta:set_int("pattern_id" = pattern_id)
+				local meta = minetest.get_meta(pos)
+				meta:set_int("pattern_id", pattern_id)
 			end
 			
 			--add the cell list to the active cell registry with the ttl, rules hash, and cell list
-			local values = {iteration=0, last_cycle=0, rule_id=rule_id, pmin=0, pmax=0, cell_count=table.get_n(cell_list), cell_list=cell_list}
+			local values = {iteration=0, last_cycle=0, rule_id=rule_id, pmin=0, pmax=0, cell_count=table.getn(cell_list), cell_list=cell_list}
 			automata.patterns[pattern_id] = values --overwrite placeholder	
 			
 			minetest.chat_send_player(player:get_player_name(), "You activated all inactive cells!")
